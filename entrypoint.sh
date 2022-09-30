a9ea78b02a7e5c778d901823fc555fc878e863c3
dmbichko <101453211+dmbichko@users.noreply.github.com>
2022-09-30 11:57:45 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,7 @@
 echo "Hello $INPUT_MYINPUT"
 memory=$(cat /proc/meminfo)
 name=$(cat /proc/sys/kernel/hostname)
-echo "::set-output name=memory::$memory"
-echo "::set-output name=name::$name"
+# Add sections for other test for Jenkins
+#echo "::set-output name=memory::$memory"
+#echo "::set-output name=name::$name"
+echo "Machine name is " $name "Memory is " $memory 
